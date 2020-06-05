3f60d030cdf3bc3a889cc1661a189a9108e04728
Vikash Kumar <58426069+vikashryder@users.noreply.github.com>
2020-06-05 17:30:40 +0530

    Update Queries_Investigate_Relational_Database.sql

--- Queries_Investigate_Relational_Database.sql
+++ Queries_Investigate_Relational_Database.sql
@@ -1,4 +1,6 @@
-# Question 1: We want to understand more about the movies that families are watching. The following categories are considered family movies: Animation, Children, Classics, Comedy, Family and Music. Create a query that lists each movie, the film category it is classified in, and the number of times it has been rented out.
+# Question 1: We want to understand more about the movies that families are watching. The following categories are considered family 
+# movies: Animation, Children, Classics, Comedy, Family and Music. Create a query that lists each movie, the film category it is 
+# classified in, and the number of times it has been rented out.
 
 SELECT f.title,
        c.name,
@@ -19,7 +21,10 @@ GROUP BY 1,
 ORDER BY 2, 1;
 
 
-# Question 2: Now we need to know how the length of rental duration of these family-friendly movies compares to the duration that all movies are rented for. Can you provide a table with the movie titles and divide them into 4 levels (first_quarter, second_quarter, third_quarter, and final_quarter) based on the quartiles (25%, 50%, 75%) of the rental duration for movies across all categories? Make sure to also indicate the category that these family-friendly movies fall in to.
+# Question 2: Now we need to know how the length of rental duration of these family-friendly movies compares to the duration that all 
+# movies are rented for. Can you provide a table with the movie titles and divide them into 4 levels 
+# (first_quarter, second_quarter, third_quarter, and final_quarter) based on the quartiles (25%, 50%, 75%) of the rental duration for 
+# movies across all categories? Make sure to also indicate the category that these family-friendly movies fall in to.
 
 SELECT f.title,
        c.name,
@@ -35,7 +40,9 @@ SELECT f.title,
 ORDER BY 3;
 
 
-# Question 3: Finally, provide a table with the family-friendly film category, each of the quartiles, and the corresponding count of movies within each combination of film category for each corresponding rental duration category. The resulting table should have three columns:
+# Question 3: Finally, provide a table with the family-friendly film category, each of the quartiles, and the corresponding count of 
+# movies within each combination of film category for each corresponding rental duration category. The resulting table should have three 
+# columns:
 # A. Category
 # B. Rental length category
 # C. Count
@@ -59,7 +66,9 @@ GROUP BY 1,
 ORDER BY 1, 2;
 
 
-# Question 4: We would like to know who were our top 10 paying customers, how many payments they made on a monthly basis during 2007, and what was the amount of the monthly payments. Can you write a query to capture the customer name, month and year of payment, and total payment amount for each month by these top 10 paying customers?
+# Question 4: We would like to know who were our top 10 paying customers, how many payments they made on a monthly basis during 2007, 
+# and what was the amount of the monthly payments. Can you write a query to capture the customer name, month and year of payment, and 
+# total payment amount for each month by these top 10 paying customers?
 
 SELECT DATE_TRUNC('month', p.payment_date) pay_month, 
        CONCAT(c.first_name, ' ', c.last_name) AS full_name, 
